688748d9763ec7320ea4738ff898210e8c722ac2
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-13 17:34:32 -0300

    feat: add BookDTO and validations

---
+++ src/books/book/book.dto.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Type } from 'class-transformer';
+import {
+  ArrayMaxSize,
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Matches,
+  MaxLength,
+} from 'class-validator';
+
+export class BookDTO {
+  @IsString()
+  @IsNotEmpty({ message: 'Title is required.' })
+  @MaxLength(254, { message: 'Title must have at most 254 characters.' })
+  tile: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Author is required.' })
+  @MaxLength(254, { message: 'Author must have at most 254 characters.' })
+  author: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/\.(jpg|jpeg|png)$/i, {
+    message: 'Image must be in JPG or PNG format.',
+  })
+  imageUrl?: string;
+
+  @IsNumber()
+  @IsPositive({ message: 'Price must be greater than zero.' })
+  @Type(() => Number)
+  price: number;
+
+  @IsOptional()
+  @IsArray({ message: 'Categories must be in an array.' })
+  @ArrayMaxSize(3, {
+    message: 'Book can have a maximum of 3 categories.',
+  })
+  @IsInt({
+    each: true,
+    message: 'Each category must be represented by ID.',
+  })
+  @Type(() => Number)
+  categories?: number[];
+}
