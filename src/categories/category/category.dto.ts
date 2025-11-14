5ade0ab90eab715a2b03702ec253d6ef961beb8b
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-13 22:57:59 -0300

    fix: adding validation for whitespaces in categoryDTO

--- src/categories/category/category.dto.ts
+++ src/categories/category/category.dto.ts
@@ -1,11 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CategoryDTO {
+  @IsString()
   @IsNotEmpty({ message: 'Category name is required.' })
   @MinLength(1, { message: 'Category must have at least 1 character.' })
   @MaxLength(254, {
     message: 'Category name must have at most 254 characters.',
+  })
+  @Matches(/^(?!\s+$).+$/, {
+    message: 'Category name cannot be only whitespace.',
   })
   name: string;
 }
