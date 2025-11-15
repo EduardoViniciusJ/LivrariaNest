a45ace1ec00cf4725868fc9fc561b039a0409aa5
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-15 19:10:50 -0300

    feat: add getById, put and delete for book

--- src/books/book/book.controller.ts
+++ src/books/book/book.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookDTO } from './book.dto';
 import { Book } from 'src/db/entities/book.entity';
@@ -17,7 +26,33 @@ export class BookController {
   // Get all books
   // Endpoint: GET api/book
   @Get()
-  async getAllBook(): Promise<Book[]>{
+  async getAllBook(): Promise<Book[]> {
     return this.bookService.getAllBook();
+  }
+
+  // Get book by ID
+  // Endpoint: GET api/book/:id
+  @Get(':id')
+  async getBookById(@Param('id', ParseIntPipe) id: number): Promise<Book> {
+    return this.bookService.getBookById(id);
+  }
+
+  // Update book by ID
+  // Endpoint: PUT api/book/update/:id
+  @Put('update/:id')
+  async updateBook(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() bookDTO: BookDTO,
+  ): Promise<Book> {
+    return this.bookService.updatebook(id, bookDTO);
+  }
+
+  // Delete book by ID
+  // Endpoint: DELETE api/book/delete/:id
+  @Delete('delete/:id')
+  async deleteBook(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<{ message: string }> {
+    return this.bookService.deleteBook(id);
   }
 }
--- src/books/book/book.service.ts
+++ src/books/book/book.service.ts
@@ -46,4 +46,48 @@ export class BookService {
     }
     return books;
   }
+
+  async getBookById(id: number): Promise<Book> {
+    const book = await this.bookRepository.findOne({
+      where: { id },
+      relations: ['categories'],
+    });
+
+    if (book == null) {
+      throw new HttpException('Book not found', HttpStatus.NOT_FOUND);
+    }
+
+    return book;
+  }
+
+  async deleteBook(id: number): Promise<{ message: string }> {
+    const book = await this.getBookById(id);
+    await this.bookRepository.remove(book);
+
+    return { message: `Book '${book.title}' deleted successfully.` };
+  }
+
+  async updatebook(id: number, bookDTO: BookDTO): Promise<Book> {
+    const book = await this.getBookById(id);
+
+    const { categories: categoryIds, ...data } = bookDTO;
+
+    const foundCategories = await this.categoryRepository.findBy({
+      id: In(categoryIds),
+    });
+
+    if (foundCategories.length !== categoryIds.length) {
+      throw new HttpException(
+        'One or more categories were not found.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const updateBook = this.bookRepository.merge(book, {
+      ...data,
+      categories: foundCategories,
+    });
+
+    return this.bookRepository.save(updateBook);
+  }
 }
